ebe63a6ed959c68480558b2d91817cbd9223ccaa
jamisliao <jamisliao@91app.com>
2016-03-16 15:44:25 +0800

    加入專案檔案。

---
+++ IOCandDI/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCandDI
+{
+    public class Class1
+    {
+    }
+}
